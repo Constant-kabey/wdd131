5dccf8ddbfe5a4daeb04d1555a9c30d11883f22e
Constant-kabey <kab20002@byui.edu>
2024-09-28 23:20:01 +0100

    From flex to grid

--- week02/scripts/temple.js
+++ week02/scripts/temple.js
@@ -1,12 +1,24 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".buttons");
 
-hamburger.addEventListener("click", ()=>{
+hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
 
-document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", () =>{
+document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active")
     navMenu.classList.remove("active")
 }))
+
+const lastmodified= document.querySelector("#lastmod");
+
+// use the date object
+const today = new Date();
+
+lastmodified.innerHTML = `Last modified on <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "full"
+	}
+).format(today)}</span>`;
